340a69feb140f844b272e890b9dbe7e360a52c46
iddiiizzzz <123089469+iddiiizzzz@users.noreply.github.com>
2026-04-04 13:03:15 +0200

    Modify crawl animation duration to 200 seconds
    Updated crawl animation duration and adjusted related functions.

--- functions.js
+++ functions.js
@@ -19,7 +19,7 @@ function runCrawl() {
   crawl.style.animation = 'none';
   crawl.offsetHeight; // force reflow
   setTimeout(() => {
-    crawl.style.animation = 'crawl 120s linear forwards';
+    crawl.style.animation = 'crawl 200s linear forwards';
     crawl.style.animationPlayState = 'running';
   }, 7000); // second delay
 
